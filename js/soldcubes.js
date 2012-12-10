605ee46e64bc99ea68bf50a4902f09efb41227b2
Alexander <knuxus@gmail.com>
2012-12-10 19:46:07 +0000

    third final edit

--- js/soldcubes.js
+++ js/soldcubes.js
@@ -1,4 +1,9 @@
 //DATABASE
 var database = Array();
-database.push([400, "João Pires", ""]);
+database.push([6, "ISCTE", ""]);database.push([17, "ISCTE", ""]);database.push([35, "ISCTE", ""]);database.push([85, "ISCTE", ""]);database.push([112, "ISCTE", ""]);
+database.push([148, "ISCTE", ""]);database.push([151, "ISCTE", ""]);database.push([203, "ISCTE", ""]);database.push([226, "ISCTE", ""]);database.push([246, "ISCTE", ""]);database.push([274, "ISCTE", ""]);database.push([326, "ISCTE", ""]);
+
+database.push([153, "FabLab ", ""]);database.push([172, "FabLab ", ""]);database.push([192, "FabLab ", ""]);database.push([204, "FabLab ", ""]);database.push([175, "FabLab ", ""]);database.push([212, "FabLab ", ""]);
+database.push([189, "FabLab ", ""]);database.push([233, "FabLab ", ""]);database.push([276, "FabLab ", ""]);database.push([261, "FabLab ", ""]);
+
 //END DATABASE
