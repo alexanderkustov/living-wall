d1e74877940528fc954952eb84d1e61a267d2a3d
Alexander <knuxus@gmail.com>
2012-12-14 14:04:24 +0000

    soldcubes organizado
    acho que falta um cubo de fablab, 212

--- js/soldcubes.js
+++ js/soldcubes.js
@@ -1,11 +1,28 @@
 //DATABASE
 var database = Array();
-database.push(["C043", "ISCTE", ""]);database.push(["C032", "ISCTE", ""]);database.push(["C018", "ISCTE", ""]);database.push(["C006", "ISCTE", ""]);database.push(["C019", "ISCTE", ""]);
-database.push(["C048", "ISCTE", ""]);database.push(["C098", "ISCTE", ""]);database.push(["C030", "ISCTE", ""]);database.push(["C042", "ISCTE", ""]);database.push(["C060", "ISCTE", ""]);database.push(["C082", "ISCTE", ""]);database.push(["C108", "ISCTE", ""]);
-
-database.push(["C010", "FabLab ", ""]);database.push(["C035", "FabLab ", ""]);database.push(["C117", "FabLab ", ""]);database.push(["C061", "FabLab ", ""]);database.push(["C096", "FabLab ", ""]);database.push([212, "FabLab ", ""]);
-database.push(["C024", "FabLab ", ""]);database.push(["C041", "FabLab ", ""]);database.push(["C079", "FabLab ", ""]);database.push(["C068", "FabLab ", ""]);
-
+database.push(["C043", "ISCTE", ""]);
+database.push(["C032", "ISCTE", ""]);
+database.push(["C018", "ISCTE", ""]);
+database.push(["C006", "ISCTE", ""]);
+database.push(["C019", "ISCTE", ""]);
+database.push(["C048", "ISCTE", ""]);
+database.push(["C098", "ISCTE", ""]);
+database.push(["C030", "ISCTE", ""]);
+database.push(["C042", "ISCTE", ""]);
+database.push(["C060", "ISCTE", ""]);
+database.push(["C082", "ISCTE", ""]);
+database.push(["C108", "ISCTE", ""]);
+//fablab
+database.push(["C010", "FabLab ", ""]);
+database.push(["C035", "FabLab ", ""]);
+database.push(["C117", "FabLab ", ""]);
+database.push(["C061", "FabLab ", ""]);
+database.push(["C096", "FabLab ", ""]);
+//database.push([212, "FabLab ", ""]);
+database.push(["C024", "FabLab ", ""]);
+database.push(["C041", "FabLab ", ""]);
+database.push(["C079", "FabLab ", ""]);
+database.push(["C068", "FabLab ", ""]);
 database.push(["V017", "Almeida Guerra ", ""]);
 database.push(["C037", "André Campino", ""]);
 database.push(["A042", "Xiou Wen ", ""]);
@@ -17,8 +34,6 @@ database.push(["A006", "Tongyan Wang ", ""]);
 database.push(["A004", "Lihong Gu ", ""]);
 database.push(["A052", "Hui He", ""]);
 database.push(["C065", "Daniel Nogueira ", ""]);
-
-//3º envio
 database.push(["C029", "Sara Eloy ", ""]);
 database.push(["C038", "Pedro Carvalho ", ""]);
 database.push(["L039", "João Batista ", ""]);
@@ -26,20 +41,14 @@ database.push(["C009", "Olinda Gama ", ""]);
 database.push(["C128", "Paula Rodrigues ", ""]);
 database.push(["C069", "Ana Costa ", ""]);
 database.push(["C130", "Diana Costa ", ""]);
-
-//2º envio
 database.push(["L013", "Maria João Simas", ""]);
 database.push(["C062", "João Pinote ", ""]);
 database.push(["L005", "Luis Meneses", ""]);
-
-// 4º envio
 database.push(["A084", "João Peniche Rosa", ""]);
 database.push(["A116", "Braulio Alturas", ""]);
 database.push(["A111", " ", ""]);
 database.push(["C002", "Maria Glória Lopes ", ""]);
-
 database.push(["C011", "Amigo secreto da Audax ", ""]);
-
 database.push(["A008", "Luciano Leal ", ""]);
 database.push(["C013", "Ana Madeira ", ""]);
 database.push(["C014", "Ana Madeira ", ""]);
@@ -47,4 +56,5 @@ database.push(["C036", "Susana Trigo Cordeiro ", ""]);
 database.push(["C055", "Carlos Glórias Ferreira ", ""]);
 database.push(["C001", "Margarida Garrido ", ""]);
 database.push(["V013", "Mazars e Associados ", ""]);
-//sexta manha
+database.push(["C059", "Manuela Farrobilha ", ""]);
+database.push(["C056", "Ana Siemaes ", ""]);
